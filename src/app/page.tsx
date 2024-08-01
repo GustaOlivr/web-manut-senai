0eb6a99999512fe6162e176ea3efb90df84d691e
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-07-31 22:43:50 -0300

    fix: fix page layout
    fix page layout and table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,40 @@ async function getData(): Promise<Payment[]> {
       status: "pending",
       email: "m@example.com",
     },
+    {
+      id: "452kkdiw1",
+      amount: 100,
+      status: "processing",
+      email: "admin@example.com",
+    },
+    {
+      id: "452kkdiw1",
+      amount: 100,
+      status: "processing",
+      email: "admin@example.com",
+    },
+    {
+      id: "452kkdiw1",
+      amount: 100,
+      status: "processing",
+      email: "admin@example.com",
+    },
+    {
+      id: "452kkdiw1",
+      amount: 100,
+      status: "processing",
+      email: "admin@example.com",
+    },
     // ...
   ];
 }
 
 export default function Home() {
   const cards = [
-    { qty: "10", text: "Ambientes", icon: <FiBox size={48} /> },
-    { qty: "78", text: "Equipamentos", icon: <FaGear size={48} /> },
-    { qty: "22", text: "O.S. Abertas", icon: <GrNotes size={48} /> },
-    { qty: "93", text: "O.S. Concluídas", icon: <IoMdCheckboxOutline size={48} /> },
+    { qty: "10", text: "Ambientes", icon: <FiBox size={40} /> },
+    { qty: "78", text: "Equipamentos", icon: <FaGear size={40} /> },
+    { qty: "22", text: "O.S. Abertas", icon: <GrNotes size={40} /> },
+    { qty: "93", text: "O.S. Concluídas", icon: <IoMdCheckboxOutline size={40} /> },
   ];
 
   const [data, setData] = React.useState<Payment[]>([]);
@@ -43,16 +67,16 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex flex-col overflow-y-auto w-full">
       <div className="flex-1 flex">
-        <Aside />
+        
 
         <main className="flex-1 flex flex-col">
           <h1 className="text-4xl font-bold uppercase w-full p-6 text-center">
             Sistema de Gestão de Manutenção
           </h1>
 
-          <div className="grid grid-cols-4 gap-4 p-6">
+          <div className="grid grid-cols-4 gap-4 p-4">
             {cards.map((props) => (
               <Card 
                 key={props.text} // Adicionei uma chave única aqui para evitar avisos do React.
@@ -63,13 +87,12 @@ export default function Home() {
             ))}
           </div>
 
-          <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={data} />
+          <div className="container mx-auto py-5">
+              <DataTable columns={columns} data={data} />
           </div>
         </main>
       </div>
 
-      <Footer />
     </div>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ interface CardInterface {
 
 export function Card({color, qty, text, icon}: CardInterface) {
     return (
-        <div className={`p-6 flex gap-2 rounded-xl ${color ? color : 'bg-muted'}`}>
+        <div className={`p-2 flex gap-2 rounded-xl ${color ? color : 'bg-muted'} items-center`}>
             <div className="flex-1 flex flex-col">
                 <strong className="text-3xl font-bold text-muted-foreground">{qty}</strong>
                 <span className="text-sm text-muted-foreground">{text}</span>
