82184b53e0e7f243e7ef235e25a3cacd6f8859f3
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-08-01 22:27:28 -0300

    feat: add maintenance page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,22 +2,22 @@ import { FaRegUserCircle} from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
 import { FiBox, FiTool } from "react-icons/fi";
 import { MdDashboard} from "react-icons/md";
-
+import Link from "next/link";
 
 export function NavBar() {
 
     const links = [
-        {target: "#", text: "Dashboard", icon: <MdDashboard />},
-        {target: "#", text: "Ambientes", icon: <FiBox />},
-        {target: "#", text: "Equipamentos", icon: <FaGear />},
-        {target: "#", text: "Manutenções", icon: <FiTool />},
-        {target: "#", text: "Usuários", icon: <FaRegUserCircle />},
+        {target: "/", text: "Dashboard", icon: <MdDashboard className="text-muted-foreground"/>},
+        {target: "#", text: "Ambientes", icon: <FiBox className="text-muted-foreground"/>},
+        {target: "#", text: "Equipamentos", icon: <FaGear className="text-muted-foreground"/>},
+        {target: "/manutencoes", text: "Manutenções", icon: <FiTool className="text-muted-foreground"/>},
+        {target: "#", text: "Usuários", icon: <FaRegUserCircle className="text-muted-foreground"/>},
     ];
 
     return (
         <nav className="space-y-4 flex flex-col bg-muted rounded-lg p-8 mt-8">
             {links.map((obj,index) => (
-                <a key={index} href={obj.target} className="flex items-center gap-4 text-secondary hover:font-semibold">
+                <a key={index} href={obj.target} className="flex items-center gap-4 text-secondary-foreground hover:font-semibold">
                     {obj.icon}
                     {obj.text}
                 </a>
