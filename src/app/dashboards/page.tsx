638850f07d5030622c6d1bb848ce17017299f0e3
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-08-26 22:21:41 -0300

    feat: add dashboard strucuture page

---
+++ src/app/dashboards/page.tsx
@@ -0,0 +1,46 @@
+"use client"; // Adicione esta linha
+
+import React from "react";
+import { Aside } from "@/components/aside";
+import { Card } from "@/components/card";
+import { Footer } from "@/components/footer";
+import { FaGear } from "react-icons/fa6";
+import { FiBox } from "react-icons/fi";
+import { GrNotes } from "react-icons/gr";
+import { IoMdCheckboxOutline } from "react-icons/io";
+
+export default function Dashboards() {
+
+  const cards = [
+    { qty: "10", text: "Ambientes", icon: <FiBox size={40} /> },
+    { qty: "78", text: "Equipamentos", icon: <FaGear size={40} /> },
+    { qty: "22", text: "O.S. Abertas", icon: <GrNotes size={40} /> },
+    {
+      qty: "93",
+      text: "O.S. Concluídas",
+      icon: <IoMdCheckboxOutline size={40} />,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col overflow-y-auto w-full">
+      <div className="flex-1 flex">
+        <main className="flex-1 flex flex-col">
+          <h1 className="text-4xl font-bold uppercase w-full p-6 text-center">
+            Sistema de Gestão de Manutenção
+          </h1>
+          <div className="grid grid-cols-4 gap-4 p-4">
+            {cards.map((props) => (
+              <Card
+                key={props.text} 
+                qty={props.qty}
+                text={props.text}
+                icon={props.icon}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
